40f0a1d1545d40400aa33c3bd59bc5697f615f25
Noverload <dune1755@outlook.com>
2024-01-02 22:31:37 +0800

    web-status-checker-version-01

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "Healthchecker",
+		Usage: "A tiny tool that checks whehter a website is running or is down",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "domain",
+				Aliases:  []string{"d"},
+				Usage:    "Domain name to check",
+				Required: false,
+			},
+			&cli.StringFlag{
+				Name:     "port",
+				Aliases:  []string{"p"},
+				Usage:    "Port number to check",
+				Required: false,
+			},
+		},
+		Action: func(c *cli.Context) error {
+			port := c.String("port")
+
+			if c.String("port") == "" {
+				port = "80"
+			}
+			result := Check(c.String("domain"), port)
+			fmt.Println(result)
+
+			return nil
+		},
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
